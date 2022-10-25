2a72989edd58722789735b200143f78415d11527
Pavel Principex <pprincipe.cons@outlook.com>
2022-10-24 23:01:51 -0500

    tercera subida subida

--- src/main/java/com/example/zdemorepo/ZdemorepoApplication.java
+++ src/main/java/com/example/zdemorepo/ZdemorepoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZdemorepoApplication {
-	//desaarrollado por pavel principe4545
+	//desaarrollado por pavel principe454567
 	public static void main(String[] args) {
 		SpringApplication.run(ZdemorepoApplication.class, args);
 	}
