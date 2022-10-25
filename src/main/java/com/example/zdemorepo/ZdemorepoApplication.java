1371a8a8204fedd7df327a826794147b0ebd98f2
Pavel Principex <pprincipe.cons@outlook.com>
2022-10-24 22:10:07 -0500

    Primera subida

---
+++ src/main/java/com/example/zdemorepo/ZdemorepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zdemorepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdemorepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdemorepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zdemorepo/ZdemorepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zdemorepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdemorepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
